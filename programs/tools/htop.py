efa3ea2d3b9db93b9029348551c9404917dfbe06
grkajac <aleksa888@gmail.com>
2018-03-24 12:42:39 +0100

    Added program dirs & init scripts

---
+++ programs/tools/htop.py
@@ -0,0 +1,2 @@
+
+print "hello from" __file__
---
+++ utils/run-command.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+import subprocess
+import sys
+
+def aca():
+    print "hello from ",  __name__
+
+if __name__ == '__main__':
+    aca()
